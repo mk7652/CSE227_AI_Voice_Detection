a90c16ce86eddd62d488c097c2b287274e995208
Ze Sun <zesun@ucsd.edu>
2024-11-10 17:06:42 -0800

    Preprocessing done.

---
+++ pre_proc.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+charsiu_dir = "charsiu"
+
+if not os.path.isdir(charsiu_dir):
+    os.system("git clone https://github.com/lingjzhu/charsiu")
+
+pre_proc_script = "pre_proc.ipynb"
+
+if not os.path.isfile(charsiu_dir + '\\' + pre_proc_script):
+    shutil.move(pre_proc_script, charsiu_dir)
+
+os.chdir('charsiu')
+
+os.system("python dummy.py")
